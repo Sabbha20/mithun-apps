4d6509dc42b281f928f39ed67198dabe11a71bb6
Sabbha20 <sabbha.power1@gmail.com>
2024-09-10 13:54:26 +0530

    Created the UI in Taipy GUi

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-from taipy.gui import Gui
+from taipy.gui import Gui, notify #, State
 
 import os
 import google.generativeai as genai
@@ -16,3 +16,34 @@ model = genai.GenerativeModel("gemini-pro")
 def get_gemini_response(query):
     resp = model.generate_content(query)
     return resp.text
+
+# Function to handle user input and update the response
+def btn_submit(state):
+    state.response = get_gemini_response(state.query)
+    notify(state, "success", "Response generated!")
+
+# Our Taipy gui
+page = """
+# Gemini LLM Application
+
+Enter your prompt:
+<|{query}|input|>
+
+<|Submit|button|on_action=btn_submit|>
+
+Response:
+<|{response}|>
+
+
+"""
+
+# def on_init(state: State):
+#     state.query = ""
+#     state.response = ""
+
+if __name__ == "__main__":
+    query = ""
+    response = ""
+    Gui(page).run(debug=True, port=8000, title="Q&A App")
+
+
